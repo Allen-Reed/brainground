c4bb1fae57aaabec938d47c50c39243cbede9040
Allen Reed <86163102+Allen-Reed@users.noreply.github.com>
2023-07-14 05:14:57 -0500

    Add spin animation

--- src/lib/components/Brain.svelte
+++ src/lib/components/Brain.svelte
@@ -4,30 +4,39 @@
   import { spring } from 'svelte/motion'
 
   let rotation = 0
-  useFrame((state, delta) => {
-    rotation += delta
+  let multiplier = 0.02
+
+  export const ssr = false
+
+  useFrame(() => {
+    rotation += multiplier
   })
 
   interactivity()
   const scale = spring(1)
+
+  function spin() {
+    multiplier = 0.05
+    setTimeout(() => {
+      multiplier = 0.02
+    }, 800)
+  }
 </script>
 
 <Environment path="/hdr/" files="desert.hdr" />
 
 <T.PerspectiveCamera makeDefault position={[4, 10, 4]} fov={50}>
-  <OrbitControls autoRotate enableDamping />
+  <OrbitControls enableDamping />
 </T.PerspectiveCamera>
 
 <T.AmbientLight />
 
-<GLTF url="/models/braincenteredscaledpink.glb" />
-
-<!-- <T.Mesh
-  rotation.y={rotation}
-  scale={$scale}
-  on:pointerenter={() => scale.set(1.5)}
-  on:pointerleave={() => scale.set(1)}
-  castShadow>
-  <T.BoxGeometry args={[2, 2, 2]} />
-  <T.MeshStandardMaterial color="hotpink" />
-</T.Mesh> -->
+<div>
+  <GLTF
+    url="/models/braincenteredscaledpink.glb"
+    rotation.y={rotation}
+    scale={$scale}
+    on:click={spin}
+    on:pointerenter={() => scale.set(1.1)}
+    on:pointerleave={() => scale.set(1)} />
+</div>

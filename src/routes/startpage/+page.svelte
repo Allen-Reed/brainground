b2d450f5cf2cac5a199eec05ad73c13d6f609563
Allen Reed <86163102+Allen-Reed@users.noreply.github.com>
2023-07-17 13:45:44 -0500

    Add startpage

---
+++ src/routes/startpage/+page.svelte
@@ -0,0 +1,56 @@
+<script>
+  import 'animate.css'
+</script>
+
+<div class=" grid xl:grid-cols-3 mx-auto h-full w-2/3">
+  <div
+    class="flex justify-center p-10 flex-col animate__animated animate__fadeInUp animate__faster">
+    <div
+      class="flex justify-center text-4xl font-heading-token w-full dark:text-primary-300 text-primary-500 mb-4">
+      ideas
+    </div>
+    <div class="font-semibold text-canter text-lg text-center">Looking for something to do?</div>
+    <p class="mt-2 h-20 text-center italic dark:text-surface-100 text-surface-700">
+      Spin the wheel of action to generate an activity for you alone or up to three friends!
+    </p>
+    <img src="images/brain-idea.svg" alt="brain-idea" class="h-80 scale-x-[-1]" />
+    <div class="flex w-full justify-center">
+      <button class="btn variant-filled-primary font-heading-token text-lg mt-2 w-48"
+        >get motivated!</button>
+    </div>
+  </div>
+  <div class="flex justify-center p-10 flex-col animate__animated animate__fadeInUp animate__fast">
+    <div
+      class="flex justify-center text-4xl font-heading-token w-full dark:text-tertiary-300 text-tertiary-500 mb-4">
+      thinkers
+    </div>
+    <div class="font-semibold text-canter text-lg text-center">
+      A trove of curiosities for the mind!
+    </div>
+    <p class="mt-2 h-20 text-center italic dark:text-surface-100 text-surface-700">
+      No need to succumb to boredom. Hop in here for riddles, jokes, and random facts.
+    </p>
+    <img src="images/brain-thinking.svg" alt="brain-thinking" class="h-80" />
+    <div class="flex w-full justify-center">
+      <button class="btn variant-filled-tertiary font-heading-token text-lg mt-1 w-48"
+        >break boredom!</button>
+    </div>
+  </div>
+  <div class="flex justify-center p-10 flex-col animate__animated animate__fadeInUp">
+    <div
+      class="flex justify-center text-4xl font-heading-token w-full dark:text-error-300 text-error-500 mb-4">
+      workouts
+    </div>
+    <div class="font-semibold text-canter text-lg text-center">
+      It's time to flex that mind meat!
+    </div>
+    <p class="mt-2 h-20 text-center italic dark:text-surface-100 text-surface-700">
+      Break a sweat on some math problems, puzzles, or play some chess against an AI!
+    </p>
+    <img src="images/brain-workout.svg" alt="brain-workout" class="h-80 scale-x-[-1]" />
+    <div class="flex w-full justify-center">
+      <button class="btn variant-filled-error font-heading-token text-lg mt-2 w-48"
+        >pump neurons!</button>
+    </div>
+  </div>
+</div>

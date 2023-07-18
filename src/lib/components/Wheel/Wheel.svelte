6b02461b65501a0a36b31d4ce5efa6b2f5b84cda
Allen Reed <86163102+Allen-Reed@users.noreply.github.com>
2023-07-18 10:59:07 -0500

    Add Wheel of Activities

---
+++ src/lib/components/Wheel/Wheel.svelte
@@ -0,0 +1,90 @@
+<script>
+  import './wheel.css'
+  import { onMount, createEventDispatcher } from 'svelte'
+
+  const dispatch = createEventDispatcher()
+
+  function handleClick() {
+    dispatch('call')
+  }
+
+  export let category = '???'
+  export let hidden = false
+  export let color
+  let disabled = false
+  let container
+  let rotationAngle = 45
+  let index
+  let divs
+  let categories = [
+    'music',
+    'relaxation',
+    'cooking',
+    'charity',
+    'diy',
+    'social',
+    'recreational',
+    'education'
+  ]
+
+  let colorMap = {
+    education: '#3f51b5',
+    recreational: '#ff9800',
+    social: '#e91e63',
+    diy: '#4caf50',
+    charity: '#009688',
+    cooking: '#795548',
+    relaxation: '#9c27b0',
+    music: '#f44336'
+  }
+
+  onMount(() => {
+    container = document.querySelector('.container')
+    divs = document.querySelectorAll('.container > div')
+  })
+
+  function getDiv() {
+    index = Math.floor(Math.random() * divs.length)
+    category = categories[index]
+    color = colorMap[category]
+    categories.unshift(...categories.splice(index, categories.length - index))
+  }
+
+  function spin() {
+    hidden = true
+    disabled = true
+    getDiv()
+    rotationAngle += index * 405
+    container.style.transform = `rotate(${rotationAngle}deg)`
+    setTimeout(() => {
+      hidden = false
+      disabled = false
+    }, 5000)
+  }
+</script>
+
+<div class="w-1/3 my-auto">
+  <div class="flex justify-center mb-2">
+    <iconify-icon icon="icomoon-free:arrow-down" width="60" style="color: white;" />
+  </div>
+  <div
+    class="container"
+    on:keydown
+    on:click={() => {
+      if (!disabled) {
+        spin()
+        handleClick()
+      }
+    }}
+    role="button"
+    tabindex="0">
+    <div class="education"><iconify-icon icon="octicon:book-16" width="60" /></div>
+    <div class="recreational"><iconify-icon icon="bx:happy" width="60" /></div>
+    <div class="social"><iconify-icon icon="raphael:people" width="60" /></div>
+    <div class="diy"><iconify-icon icon="ion:hammer" width="60" /></div>
+    <div class="charity"><iconify-icon icon="mdi:charity" width="60" /></div>
+    <div class="cooking"><iconify-icon icon="la:cookie-bite" width="60" /></div>
+    <div class="relaxation"><iconify-icon icon="material-symbols:chair" width="60" /></div>
+    <div class="music"><iconify-icon icon="majesticons:music" width="60" /></div>
+  </div>
+</div>
---
+++ src/routes/ideas/+page.svelte
@@ -0,0 +1,52 @@
+<script>
+  import Wheel from '../../lib/components/Wheel/Wheel.svelte'
+  import 'animate.css'
+  import { RadioGroup, RadioItem } from '@skeletonlabs/skeleton'
+
+  let category
+  let color
+  let hidden
+  let disabled
+  let activity = '???'
+
+  async function fetchActivity() {
+    try {
+      const response = await fetch(`http://www.boredapi.com/api/activity?type=${category}`)
+      const data = await response.json()
+      activity = data.activity
+    } catch (error) {
+      console.error(error)
+    }
+  }
+</script>
+
+<div class="h-full flex">
+  <div class="flex justify-center p-10 flex-col animate__animated animate__backInLeft ml-36">
+    <img src="images/brain-idea.svg" alt="brain-idea" class="h-[30rem] w-[30rem] scale-x-[-1]" />
+  </div>
+  <Wheel bind:category bind:hidden bind:color on:call={fetchActivity} />
+  <div class="w-2/5 mr-36 my-auto">
+    <div class="flex justify-center font-heading-token text-3xl">spin the wheel of activities!</div>
+    <div
+      class="flex justify-center font-heading-token text-4xl mt-4 animate__animated animate__fadeIn bg-white dark:bg-surface-100 rounded-full w-96 mx-auto py-2 text-surface-500">
+      {#if hidden}
+        <div>???</div>
+      {:else}
+        <div class="text-[var(--text-color)]" style="--text-color: {color}">
+          {category}
+        </div>
+      {/if}
+    </div>
+    <div
+      class="flex justify-center font-heading-token text-2xl mt-16 dark:text-primary-300 text-primary-500">
+      suggestion:
+    </div>
+    {#if !hidden}
+      <div class="flex justify-center font-heading-token text-5xl mt-2 text-center max-w-80">
+        {activity}
+      </div>
+    {:else}
+      <div class="flex justify-center font-heading-token text-5xl mt-2 text-center">???</div>
+    {/if}
+  </div>
+</div>

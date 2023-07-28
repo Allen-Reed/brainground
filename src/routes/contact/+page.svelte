3af02956ec467886703fd322bc5b9a16f9a40d42
Allen Reed <86163102+Allen-Reed@users.noreply.github.com>
2023-07-28 15:41:41 -0500

    Add contact page

---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,8 @@
+<div class="w-[40rem] flex m-auto mt-20">
+  <img src="images/AllenPhoto1.jpg" alt="pure handsomness" class="w-96 border-4" />
+  <div class="ml-6 w-full">
+    <div class="text-5xl w-96 font-heading-token">allen reed</div>
+    <div class="text-xl">allenduanereed@gmail.com</div>
+  </div>
+</div>
+<div class="text-3xl p-6 text-center">Short bio</div>

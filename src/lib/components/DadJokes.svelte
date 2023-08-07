50eb9949fea9e47ededf3e28a46fe9db4e2c8023
Allen Reed <86163102+Allen-Reed@users.noreply.github.com>
2023-08-07 17:42:55 -0500

    Set joke text size break point

--- src/lib/components/DadJokes.svelte
+++ src/lib/components/DadJokes.svelte
@@ -30,7 +30,7 @@
 
 <div class="h-1/3 animate__animated animate__fadeInUp w-full">
   <div class="flex justify-center text-warning-600 dark:text-warning-400 text-5xl mt-4">JOKE:</div>
-  <div class="flex justify-center mt-4 text-3xl">
+  <div class="flex justify-center mt-4 text-xl 2xl:text-3xl">
     <div class="w-2/3 text-center">{joke}</div>
   </div>
   <div class="flex justify-center">
